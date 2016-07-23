60c686f2c6b5697186bcfb69a0c40047bf828497
Nikolay Nechay <nechaii@ukr.net>
2016-07-23 12:30:11 +0300

    group by

---
+++ Lab/17_InOutOperationApp/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InOutOperationApp
+{
+    using System.IO;
+    using System.Threading;
+    class WorkWithFileData
+    {
+        string FileName { get; set; }
+        string CurrentLocation { get; set; }
+
+        public WorkWithFileData(string filePath)
+        {
+            FileInfo _fileInfo = new FileInfo(filePath);
+            FileName = _fileInfo.Name;
+            CurrentLocation = _fileInfo.DirectoryName;
+
+        }
+
+       async public void WriteToFile()
+        {
+            byte[] someString = Encoding.ASCII.GetBytes("Hellow InputOutputLibrary \n");
+
+            using (FileStream flStream = 
+                 File.Open(CurrentLocation+FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                flStream.Seek(0, SeekOrigin.End);
+                await flStream.WriteAsync(someString, 0, someString.Length);
+            }
+
+            Console.WriteLine("WriteToFile compleated!");
+        }
+
+
+        async public void ReadFromFile()
+        {
+            byte[] someString = null;
+            using (FileStream _fileStream = File.Open(CurrentLocation + FileName, FileMode.Open))
+            {
+                someString = new byte[_fileStream.Length];
+                await _fileStream.ReadAsync(someString, 0, (int)_fileStream.Length);
+            }
+            Console.WriteLine("ReadFromFile compleated!");
+            Console.WriteLine(Encoding.ASCII.GetString(someString));
+        }
+
+        public void SomeMemoryStream()
+        {
+            Console.WriteLine(new string('*', 50));
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] someStreang = Encoding.ASCII.GetBytes("Simple MemoryStream");
+                ms.Write(someStreang, 0, someStreang.Length);
+
+                Console.WriteLine("MemoryStream finish");               
+            }
+        }
+
+        public void PrintFileInfo()
+        {
+            Console.WriteLine("name: {0}, path: {1}",FileName,CurrentLocation);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            WorkWithFileData _workWithFileData = new WorkWithFileData(@"E:\TextFile1.txt");
+
+            _workWithFileData.PrintFileInfo();
+
+            Console.WriteLine("BeginAsyncOperation");
+
+            _workWithFileData.WriteToFile();
+
+            Thread.Sleep(1000);
+            Console.WriteLine(new string('-', 50));
+
+            _workWithFileData.ReadFromFile();
+
+            Thread th = new Thread(_workWithFileData.SomeMemoryStream);
+            th.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab/LabSqlQuary.sql
@@ -0,0 +1,35 @@
+﻿use Northwind;
+go
+
+/*like*/
+--1 get all customers where firs letter is 'B'
+select * from dbo.Customers c where LOWER(c.CompanyName) like lower('B%');
+
+/*alter*/
+--2 add new column to table product
+select * from dbo.Products
+alter table dbo.Products add newColumn varchar(10) default 'petro';
+
+/*inner join*/
+--SQL 87 
+select o.OrderID,o.ShipName,o.ShipAddress,o.ShipCity,od.UnitPrice,od.Quantity 
+from dbo.Orders o, dbo.[Order Details] od 
+where o.OrderID=od.OrderID;
+--SQL 92
+select o.OrderID,o.ShipName,o.ShipAddress,o.ShipCity,od.UnitPrice,od.Quantity
+ from dbo.Orders o join dbo.[Order Details] od 
+on o.OrderID=od.OrderID;
+
+ /*промежуточные итоги*/
+ --количетсво заказов с накоплением относительно года
+ select distinct Year(o.OrderDate), count(od.OrderID)
+ from dbo.Orders o join [Order Details] od on  o.OrderID>=od.OrderID
+ where o.OrderID is not null
+ group by Year(o.OrderDate);
+
+--прибыль с накоплением, итог - сумма заработанная за все года.
+ select distinct Year(o.OrderDate), sum(od.Quantity*od.UnitPrice)
+ from dbo.Orders o join [Order Details] od on  o.OrderID>=od.OrderID
+ where o.OrderID is not null
+ group by Year(o.OrderDate);
+
